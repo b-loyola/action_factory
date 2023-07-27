971409d510f495f24d476cf2c7eca47a6c2e593a
Ben Loyola <berna.loyola@gmail.com>
2023-07-26 23:30:14 -0700

    version bump

--- spec/action_factory_spec.rb
+++ spec/action_factory_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 
 RSpec.describe ActionFactory do
   it "has a version number" do
-    expect(ActionFactory::VERSION).to eq "0.1.1"
+    expect(ActionFactory::VERSION).to eq "0.1.2"
   end
 
   describe ".configuration" do
